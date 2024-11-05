c37895b334e14785530f6c7f5926124815f78741
Rahul Katara <rahulkatara@Rahuls-MacBook-Air.local>
2024-11-05 16:32:32 +0530

    done

---
+++ weark2/tempCodeRunnerFile.js
@@ -0,0 +1,29 @@
+// function setTimeoutPromisified(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+//   function setTimeoutPromisified(ms) {
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+//   }
+  
+//   setTimeoutPromisified(1000).then(function () {
+//     console.log("hi");
+//     setTimeoutPromisified(3000).then(function () {
+//       console.log("hello");
+//       setTimeoutPromisified(5000).then(function () {
+//         console.log("hello there");
+//       });
+//     });
+//   });
+  
+setTimeoutPromisified(1000)
+  .then(function () {
+    console.log("hi");
+    return setTimeoutPromisified(3000);
+  })
+  .then(function () {
+    console.log("hello");
+    return setTimeoutPromisified(5000);
+  })
+  .then(function () {
+    console.log("hello there");
+  });
